6c2b2c977ee320d8d36f53f4a334ea7aa7920249
tsq <tangshiqiangcn@gmail.com>
2023-02-19 11:44:08 +0800

    update error msg

--- watcher.js
+++ watcher.js
@@ -2,13 +2,13 @@ const chokidar = require('chokidar');
 const fs = require('fs');
 const loader = require('./loader');
 
-const WATCH_DIR = process.env.WATCH_DIR || '/app/tmp/logs'
+const WATCH_DIR = process.env.WATCH_DIR || 'app/tmp/logs'
 
 module.exports = (options) => {
-    const logDir = process.cwd() + WATCH_DIR;
+    const logDir = process.cwd() + '/' + WATCH_DIR;
 
     if (!fs.existsSync(logDir)) {
-        console.log('Error: app/tmp/logs directory is not exist');
+        console.log(`Error: ${WATCH_DIR} directory is not exist`);
         process.exit(1);
     }
 
